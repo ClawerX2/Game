883a5fa4f76a695b16970f78855836d397d109db
Gleb <gleb.gurov.jr@gmail.com>
2026-06-07 15:01:38 +0300

    Upgrade code

--- Scenes/Player/player.gd
+++ Scenes/Player/player.gd
@@ -20,12 +20,19 @@ const TO_CROUCH_SPEED = 10
 const head_pos: Vector3 = Vector3(0, 0.5, 0)
 const crouch_pos: Vector3 = Vector3(0, 0.0, 0)
 
+var speed_coeff = 1
 
 var mov_dir: Vector3 = Vector3.ZERO
 var menu: bool = false
 
-func get_speed():
-	pass
+func _calc_speed_coeff() -> float:
+	if Input.is_action_pressed("Ctrl") or ceiling_ray.is_colliding():
+		return 0.5
+	elif Input.is_action_pressed("Shift"):
+		return 1.3
+	else:
+		return 1.0
+
 
 #Если находимся в прыжке
 func _handle_air(delta: float) -> void:
@@ -36,16 +43,18 @@ func _handle_air(delta: float) -> void:
 	
 #Если идем по земле
 func _handle_ground(delta: float) -> void:
-	velocity = lerp(velocity, mov_dir * SPEED, delta * SPEED * FRIC_WEIGHT)
+	velocity = lerp(velocity, mov_dir * SPEED * speed_coeff, delta * SPEED * FRIC_WEIGHT)
 	
 	
 	
 func _crouch(delta: float, is_crouch: bool) -> void:
 	if is_crouch:
+		speed_coeff = 0.5
 		stand_col.disabled  = true
 		crouch_col.disabled = false
 		cam.position = lerp(cam.position, crouch_pos, delta*TO_CROUCH_SPEED)
 	elif not ceiling_ray.is_colliding():
+		speed_coeff = 1
 		stand_col.disabled  = false
 		crouch_col.disabled = true
 		cam.position = lerp(cam.position, head_pos, delta*TO_CROUCH_SPEED)
@@ -54,7 +63,7 @@ func _crouch(delta: float, is_crouch: bool) -> void:
 
 #Крутим камеру
 func _unhandled_input(event: InputEvent) -> void:
-	if event is InputEventMouseMotion:
+	if event is InputEventMouseMotion and not menu:
 		rotate_y(-event.relative.x * mouse_sense)
 		cam.rotate_x(-event.relative.y * mouse_sense)
 		cam.rotation.x = clamp(cam.rotation.x, deg_to_rad(MIN_PITCH), deg_to_rad(MAX_PITCH))
@@ -75,12 +84,15 @@ func _physics_process(delta: float) -> void:
 	var input_dir := Input.get_vector("MoveL", "MoveR", "MoveF", "MoveB")
 	mov_dir = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	
+	speed_coeff = _calc_speed_coeff()
+	
 	if is_on_floor():
 		_handle_ground(delta)
 		if Input.is_action_just_pressed("Jmp") and not ceiling_ray.is_colliding():
 			velocity.y = JUMP_VELOCITY
 	else:
 		_handle_air(delta)
+
 		
 	var is_ctrl_pressed = Input.is_action_pressed("Ctrl")
 	_crouch(delta, is_ctrl_pressed)
