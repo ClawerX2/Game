00aa7a4f2290f2b74e5c0d43ef0b2c12f6747d8d
Ivan <gurov.ivan5002@gmail.com>
2026-06-10 14:27:15 +0300

    Работа над инвентарем и правка багов

---
+++ Scenes/InventoryManager/inventory_manager.gd
@@ -0,0 +1,30 @@
+extends Node
+class_name InventoryManager
+
+@onready var grid: GridContainer = %GridContainer
+@onready var canvas: CanvasLayer = %CanvasLayer
+
+var slot_prefab: PackedScene = load("res://Scenes/InventoryManager/inventory_slot.tscn")
+var slots_count: int = 24
+var slots: Array[InventorySlot] = []
+var is_opened: bool = false
+
+func add_new_slot():
+	var new_slot = slot_prefab.instantiate() as InventorySlot
+	grid.add_child(new_slot)
+	slots.append(new_slot)
+
+func inv_toggle():
+	if is_opened: #если открыт, то закрываем
+		Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+		canvas.visible = false
+	else:
+		Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
+		canvas.visible = true
+		
+	is_opened = !is_opened
+
+func _ready() -> void:
+	is_opened = false
+	for i in slots_count:
+		add_new_slot()
--- Scenes/Player/player.gd
+++ Scenes/Player/player.gd
@@ -8,22 +8,22 @@ extends CharacterBody3D
 @onready var stand_col  = $StandCollision
 @onready var crouch_col = $CrouchCollision
 @onready var ceiling_ray: RayCast3D = $RayCast3D
+@onready var inventory: InventoryManager = $InventoryManager
 
 const MIN_PITCH = -90.0
 const MAX_PITCH = 90.0
+const TO_CROUCH_SPEED = 10
+const head_pos: Vector3 = Vector3(0, 0.5, 0)
+const crouch_pos: Vector3 = Vector3(0, 0.0, 0)
 
 #Можно считать это трением - чем больше, тем реще персонаж будет стартовать и останавливаться
 const FRIC_WEIGHT = 4 
 
-const TO_CROUCH_SPEED = 10
-
-const head_pos: Vector3 = Vector3(0, 0.5, 0)
-const crouch_pos: Vector3 = Vector3(0, 0.0, 0)
-
 var speed_coeff = 1
-
 var mov_dir: Vector3 = Vector3.ZERO
 var menu: bool = false
+var is_crouch: bool = false
+
 
 func _calc_speed_coeff() -> float:
 	if Input.is_action_pressed("Ctrl") or ceiling_ray.is_colliding():
@@ -37,45 +37,39 @@ func _calc_speed_coeff() -> float:
 #Если находимся в прыжке
 func _handle_air(delta: float) -> void:
 	velocity += get_gravity() * delta
-	velocity += mov_dir * SPEED * delta
+	velocity += mov_dir * SPEED * _calc_speed_coeff() * delta
 	velocity = velocity.limit_length(SPEED)
-	
-	
+
+
 #Если идем по земле
 func _handle_ground(delta: float) -> void:
 	velocity = lerp(velocity, mov_dir * SPEED * speed_coeff, delta * SPEED * FRIC_WEIGHT)
-	
-	
-	
-func _crouch(delta: float, is_crouch: bool) -> void:
-	if is_crouch:
-		stand_col.disabled  = true
-		crouch_col.disabled = false
-		cam.position = lerp(cam.position, crouch_pos, delta*TO_CROUCH_SPEED)
-	elif not ceiling_ray.is_colliding():
-		stand_col.disabled  = false
-		crouch_col.disabled = true
-		cam.position = lerp(cam.position, head_pos, delta*TO_CROUCH_SPEED)
-		
-	
 
-#Крутим камеру
+
+#Обработка любых событий каждый игровой кадр (180 раз в сек. при 180 FPS)
+func _process(delta: float) -> void:
+	if Input.is_action_just_pressed("Inventory"):
+		inventory.inv_toggle()
+		
+
+#Обработка ввода игрока (включая движение мыши)
 func _unhandled_input(event: InputEvent) -> void:
 	if event is InputEventMouseMotion and not menu:
 		rotate_y(-event.relative.x * mouse_sense)
 		cam.rotate_x(-event.relative.y * mouse_sense)
 		cam.rotation.x = clamp(cam.rotation.x, deg_to_rad(MIN_PITCH), deg_to_rad(MAX_PITCH))
 	
-	var is_esc_pressed = event.is_action_pressed("Esc")
-	if is_esc_pressed:
-		menu = not menu
+	#FIXME: режим мыши выставляется каждый раз когда происходит ввод от игрока, это мешает
+	#		инвентарю правильно работать. Нужно сделать чтобы режим мыши выставлялся один раз при нажатии Esc
+	
+	#if menu:
+	#	Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
+	#else:
+	#	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
 		
-	if menu:
-		Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
-	else:
-		Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
-
-
+	var crouch_action = Input.is_action_pressed("Ctrl")
+	if(crouch_action != is_crouch): crouch_toggle()
+	
 
 #Обработка движения каждый тик физического движка (60 раз в секунду)
 func _physics_process(delta: float) -> void:
@@ -91,16 +85,18 @@ func _physics_process(delta: float) -> void:
 	else:
 		_handle_air(delta)
 
-		
-	var is_ctrl_pressed = Input.is_action_pressed("Ctrl")
-	_crouch(delta, is_ctrl_pressed)
+	if is_crouch:
+		cam.position = lerp(cam.position, crouch_pos, delta * TO_CROUCH_SPEED)
+	else:
+		cam.position = lerp(cam.position, head_pos, delta * TO_CROUCH_SPEED)
 
 	move_and_slide()
-	
-	
-	
+
+func crouch_toggle():
+	if is_crouch and ceiling_ray.is_colliding(): return
+	is_crouch = !is_crouch
+	crouch_col.disabled = !is_crouch
+	stand_col.disabled = is_crouch
+
 func _ready() -> void:
-	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
-	crouch_col.disabled = true
-	stand_col.disabled = false
-	
+	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED	
